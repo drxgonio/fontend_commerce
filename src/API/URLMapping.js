88f57584dbb8c103847cdf12b88babed4b5e844d
dracula <vantriphan2105@gmail.com>
2020-08-09 11:15:00 +0700

    fix

--- src/API/URLMapping.js
+++ src/API/URLMapping.js
@@ -1,6 +1,8 @@
 //export const API_BASE_URL = 'https://ecommercemean.herokuapp.com';
-export const API_BASE_URL_FE = 'https://ecommercexyz.herokuapp.com/';
-export const OAUTH2_REDIRECT_URI = 'https://ecommercexyz.herokuapp.com/oauth2/redirect'
+// export const API_BASE_URL_FE = 'https://ecommercexyz.herokuapp.com/';
+// export const OAUTH2_REDIRECT_URI = 'https://ecommercexyz.herokuapp.com/oauth2/redirect'
+export const API_BASE_URL_FE = 'https://ecommerceabc.herokuapp.com/';
+export const OAUTH2_REDIRECT_URI = 'https://ecommerceabc.herokuapp.com/oauth2/redirect'
 export const API_BASE_URL = 'http://localhost:8081';
 // export const API_BASE_URL_FE = 'http://localhost:8080/';
 // export const OAUTH2_REDIRECT_URI = 'http://localhost:8080/oauth2/redirect'
