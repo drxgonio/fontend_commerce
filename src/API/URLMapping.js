3ef31ce272117a4b0430b835b287df50d9123e6c
dracula <vantriphan2105@gmail.com>
2020-08-09 14:14:04 +0700

    deploy local

--- src/API/URLMapping.js
+++ src/API/URLMapping.js
@@ -1,12 +1,12 @@
-export const API_BASE_URL = 'https://ecommercemean.herokuapp.com';
-export const API_BASE_URL_FE = 'https://ecommercepvt.herokuapp.com/';
-export const OAUTH2_REDIRECT_URI = 'https://ecommercepvt.herokuapp.com/oauth2/redirect'
+// export const API_BASE_URL = 'https://ecommercemean.herokuapp.com';
+// export const API_BASE_URL_FE = 'https://ecommercepvt.herokuapp.com/';
+// export const OAUTH2_REDIRECT_URI = 'https://ecommercepvt.herokuapp.com/oauth2/redirect'
 // export const API_BASE_URL = 'http://localhost:8081';
 // export const API_BASE_URL_FE = 'https://ecommerceabc.herokuapp.com/';
 // export const OAUTH2_REDIRECT_URI = 'https://ecommerceabc.herokuapp.com/oauth2/redirect'
-// export const API_BASE_URL = 'http://localhost:8081';
-// export const API_BASE_URL_FE = 'http://localhost:8080/';
-// export const OAUTH2_REDIRECT_URI = 'http://localhost:8080/oauth2/redirect'
+export const API_BASE_URL = 'http://localhost:8081';
+export const API_BASE_URL_FE = 'http://localhost:8080/';
+export const OAUTH2_REDIRECT_URI = 'http://localhost:8080/oauth2/redirect'
 export const ACCESS_TOKEN = 'accessToken';
 export const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
 export const FACEBOOK_AUTH_URL = API_BASE_URL + '/oauth2/authorize/facebook?redirect_uri=' + OAUTH2_REDIRECT_URI;
